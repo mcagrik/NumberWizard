719c747de97b07b6b134902bbbf2575a633570f1
Muhammet Çağrı Karakaya <mcagri.karakaya@gmail.com>
2017-04-18 15:15:40 +0300

    First Commit

---
+++ Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Number Wizarda hoşgeldiniz");
+		print("Aklından bir sayı tut ve bana söyleme!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
